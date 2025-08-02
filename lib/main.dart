1ecf8852669107bec5d4759d10c6859f5d417c57
goldenshoes001 <sebim1993@googlemail.com>
2025-08-02 17:32:24 +0200

    bilder hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Sebastian!'))),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/grafics/burger.png'),
+            Image.asset('assets/grafics/cupake_chick.png'),
+            Image.asset('assets/grafics/cupake_cat.png'),
+            Image.asset('assets/grafics/icecream_cone.png'),
+            Image.asset('assets/grafics/icecream_stick.png'),
+            Image.asset('assets/grafics/icecream.png'),
+            Image.asset('assets/grafics/star.png'),
+
+            Image.asset('assets/backgrounds/bg_mainscreen.png'),
+            Image.asset('assets/backgrounds/bg_startscreen.png'),
+
+            Image.asset('assets/details/cut_card.png'),
+            Image.asset('assets/details/snack_snack.png'),
+          ],
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'JP App',
     );
   }
 }
